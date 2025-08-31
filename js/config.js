1bef82141c5b9b6f78757e049d07a0114606c482
OLDMAN <h0048625@gmail.com>
2025-08-31 21:17:19 +0800

    fix: URL again

--- js/config.js
+++ js/config.js
@@ -3,8 +3,7 @@ export let imageVersion = null;
 
 // 讀取版本設定（或從 config json 讀取）
 export async function loadConfig() {
-  const manifestUrl = new URL('manifest.json', document.baseURI).href;
-  const configResponse = await fetch(manifestUrl);
+  const configResponse = await fetch('./manifest.json');
   const config = await configResponse.json();
   spriteBasePath = config.spriteBasePath;
 
