d9a730c3f957a1814199570ef2880c268334abb2
OLDMAN <h0048625@gmail.com>
2026-09-10 02:34:49 +0000

    Update DDragon to 16.18.1

---
+++ asset/dragontail/16.18.1/manifest.js
@@ -0,0 +1,1 @@
+Riot.DDragon.m={"n":{"item":"16.18.1","rune":"16.18.1","mastery":"16.18.1","summoner":"16.18.1","champion":"16.18.1","profileicon":"16.18.1","map":"16.18.1","language":"16.18.1","sticker":"16.18.1"},"v":"16.18.1","l":"en_US","cdn":null,"dd":"16.18.1","lg":"16.18.1","css":"16.18.1","profileiconmax":28};
